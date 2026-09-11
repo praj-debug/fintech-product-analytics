136624dd5055e433cead763bb10e90b033333504
praj-debug <prajumane114@gmail.com>
2026-09-11 20:34:50 +0530

    Add partner performance SQL analysis

--- analysis/partner_analysis.sql
+++ analysis/partner_analysis.sql
@@ -11,34 +11,30 @@ GROUP BY partner
 ORDER BY total_applications DESC;
 
 
--- 2. Approved applications by partner
+-- 2. Disbursed cases by partner
 SELECT
     partner,
-    COUNT(*) AS approved_applications
+    SUM(
+        CASE
+            WHEN disbursed = 'Yes' THEN 1
+            ELSE 0
+        END
+    ) AS disbursed_cases
 FROM fintech_lending_portfolio_dataset
-WHERE approval_status = 'Yes'
 GROUP BY partner
-ORDER BY approved_applications DESC;
+ORDER BY disbursed_cases DESC;
 
 
--- 3. Disbursed applications by partner
+-- 3. Partner approval rate
 SELECT
     partner,
-    COUNT(*) AS disbursed_applications
-FROM fintech_lending_portfolio_dataset
-WHERE disbursed = 'Yes'
-GROUP BY partner
-ORDER BY disbursed_applications DESC;
-
-
--- 4. Partner approval rate
-SELECT
-    partner,
-    COUNT(*) AS total_applications,
-    SUM(CASE WHEN approval_status = 'Yes' THEN 1 ELSE 0 END) AS approved_applications,
     ROUND(
-        100.0 * SUM(CASE WHEN approval_status = 'Yes' THEN 1 ELSE 0 END)
-        / COUNT(*),
+        100.0 * SUM(
+            CASE
+                WHEN approval_status = 'Yes' THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
         2
     ) AS approval_rate_pct
 FROM fintech_lending_portfolio_dataset
@@ -46,14 +42,16 @@ GROUP BY partner
 ORDER BY approval_rate_pct DESC;
 
 
--- 5. Partner disbursement rate
+-- 4. Partner disbursement rate
 SELECT
     partner,
-    COUNT(*) AS total_applications,
-    SUM(CASE WHEN disbursed = 'Yes' THEN 1 ELSE 0 END) AS disbursed_applications,
     ROUND(
-        100.0 * SUM(CASE WHEN disbursed = 'Yes' THEN 1 ELSE 0 END)
-        / COUNT(*),
+        100.0 * SUM(
+            CASE
+                WHEN disbursed = 'Yes' THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
         2
     ) AS disbursement_rate_pct
 FROM fintech_lending_portfolio_dataset
@@ -61,16 +59,18 @@ GROUP BY partner
 ORDER BY disbursement_rate_pct DESC;
 
 
--- 6. Average processing time by partner
+-- 5. Partner processing performance
 SELECT
     partner,
-    ROUND(AVG(processing_days), 2) AS avg_processing_days
+    COUNT(*) AS total_applications,
+    ROUND(AVG(processing_days), 2) AS avg_processing_days,
+    ROUND(AVG(loan_amount), 2) AS avg_loan_amount
 FROM fintech_lending_portfolio_dataset
 GROUP BY partner
 ORDER BY avg_processing_days ASC;
 
 
--- 7. Estimated revenue by partner
+-- 6. Partner revenue contribution
 SELECT
     partner,
     ROUND(SUM(estimated_partner_revenue), 2) AS estimated_revenue
